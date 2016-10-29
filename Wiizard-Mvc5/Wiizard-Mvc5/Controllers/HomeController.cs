8f7188144ba219d789b286295ca69e638c4de5e6
wiizard chen <wiizard-chen@foxmail.com>
2016-10-30 01:41:55 +0800

    first chapter

--- Wiizard-Mvc5/Wiizard-Mvc5/Controllers/HomeController.cs
+++ Wiizard-Mvc5/Wiizard-Mvc5/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Wiizard_Mvc5.Models;
 namespace Wiizard_Mvc5.Controllers
 {
     public class HomeController : Controller
@@ -11,7 +11,24 @@ namespace Wiizard_Mvc5.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "good Morning" : "good Afternoon";
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse gurstResponse)
+        {
+            if (ModelState.IsValid)
+                return View("thanks", gurstResponse);
+            else
+                return View();
         }
     }
 }
