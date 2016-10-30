d1822ff86fa590b34e1791f000777ca86303ed27
wiizard chen <wiizard-chen@foxmail.com>
2016-10-30 23:49:04 +0800

    nothing

---
+++ Wiizard-Mvc5/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+    }
+}
