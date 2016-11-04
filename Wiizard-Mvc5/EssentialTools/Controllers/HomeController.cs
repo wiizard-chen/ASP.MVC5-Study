0cb85a285c3db2ab65e239cef387d6c75dec91c5
wiizard chen <wiizard-chen@foxmail.com>
2016-11-04 10:52:26 +0800

    fuck again

---
+++ Wiizard-Mvc5/EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using EssentialTools.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products =
+        {
+            new Product {Name = "kayak", Category="watersports",Price=275M },
+            new Product {Name = "lifejacket", Category="watersports",Price=48.95M },
+            new Product {Name = "soccer ball", Category="soccer",Price=19.50M },
+            new Product {Name = "corner flag", Category="watersports",Price=34.95M }
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            LinqValueCalculator calc = new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calc);
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
---
+++ Wiizard-Mvc5/EssentialTools/Models/LinqValueCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class LinqValueCalculator
+    {
+        public decimal  ValueProducts(IEnumerable<Product> product)
+        {
+            return product.Sum(p => p.Price);
+        }
+    }
+}
